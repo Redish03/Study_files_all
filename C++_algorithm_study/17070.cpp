f6a25da4b17f51a993ecb3fdbdeed641ef313a9f
홍성우 <98575563+Redish03@users.noreply.github.com>
2024-08-15 16:09:44 +0900

    Update 17070.cpp

--- C++_algorithm_study/17070.cpp
+++ C++_algorithm_study/17070.cpp
@@ -1,40 +1,40 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
 int N;
+int dp[17][17][3];
 int arr[17][17];
-int dp[17][17];
-
-void calc()
-{
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            if (arr[i][j] == 1)
-            {
-                dp[i][j] = 0
-            }
-            if (i == 0 || j == 0)
-            {
-                dp[i][j] = 1;
-            }
-        }
-    }
-}
 
 int main()
 {
-    cin >> N;
-
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cin >> N; 
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
             cin >> arr[i][j];
-            dp[i][j] = 0;
+        }    
+    }
+    
+    dp[0][1][0] = 1;
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            if(i == 0 && j <= 1 || arr[i][j] == 1) continue;
+            
+            if(j != 0) {
+                dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
+            }
+            if(i != 0) {
+                dp[i][j][1] = dp[i - 1][j][2] + dp[i - 1][j][1];
+            }
+            if(i > 0 && j > 0 && arr[i - 1][j] != 1 && arr[i][j - 1] != 1) {
+                dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+            }
         }
     }
-    calc();
+    int result = dp[N - 1][N - 1][0] + dp[N - 1][N - 1][1] + dp[N - 1][N - 1][2];
+    cout << result;
 }
